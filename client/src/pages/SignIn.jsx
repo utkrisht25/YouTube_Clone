5889ecf63775d753a8f2d84de053a79feee44f87
umesh <utkrisht@gmail.com>
2025-08-12 20:20:14 +0530

    sign in api merged with the UI and working fine

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -13,10 +13,13 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Card } from "@/components/ui/card";
-import { RouteSignUp } from "@/helpers/RouteName";
-import { Link } from "react-router-dom";
+import { RouteSignUp, RouteIndex } from "@/helpers/RouteName";
+import { Link, useNavigate } from "react-router-dom";
+import { getEvn } from "@/helpers/getEnv";
+import { showToast } from "@/helpers/showToast";
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const formSchema = z.object({
     email: z.string().email(),
     password: z.string().min(6, {
@@ -30,17 +33,31 @@ const SignIn = () => {
       password: "",
     },
   });
-  function onSubmit(values) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  async function onSubmit(values) {
     console.log(values);
+    try {
+      const response = await fetch(`${getEvn("VITE_API_BASE_URL")}/auth/login`,
+        {
+          method: "post",
+          headers: { "Content-type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify(values),
+        }
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        return showToast("error", data.message);
+      }
+      navigate(RouteIndex);
+      showToast("success", data.message);
+    } catch (error) {
+      showToast("error", error.message);
+    }
   }
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <Card className="w-[400px] p-5">
-        <h1 className="text-2xl font-bold text-center">
-          Login Into Account
-        </h1>
+        <h1 className="text-2xl font-bold text-center">Login Into Account</h1>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="mb-3">
@@ -66,7 +83,11 @@ const SignIn = () => {
                   <FormItem>
                     <FormLabel>Password</FormLabel>
                     <FormControl>
-                      <Input type="password"  placeholder="Enter your password..." {...field} />
+                      <Input
+                        type="password"
+                        placeholder="Enter your password..."
+                        {...field}
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -74,13 +95,18 @@ const SignIn = () => {
               />
             </div>
             <div className="mt-5">
-               <Button type="submit" className="w-full">
-                 Sign In
-               </Button>
-               <div className="flex gap-2 text-sm my-5 items-center justify-center"> 
-                  <p>Don&apos;t have account?</p>
-                  <Link to={RouteSignUp} className="text-blue-600 hover:underline">Sign Up</Link>
-               </div>
+              <Button type="submit" className="w-full">
+                Sign In
+              </Button>
+              <div className="flex gap-2 text-sm my-5 items-center justify-center">
+                <p>Don&apos;t have account?</p>
+                <Link
+                  to={RouteSignUp}
+                  className="text-blue-600 hover:underline"
+                >
+                  Sign Up
+                </Link>
+              </div>
             </div>
           </form>
         </Form>
