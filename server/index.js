c124c843a361515bd2e7c88a49a0b843b1f66ffe
umesh <utkrisht@gmail.com>
2025-08-12 14:45:49 +0530

    create the error hanlder explicitly to use it as a middleware in next()

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials: true
