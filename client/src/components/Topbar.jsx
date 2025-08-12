e67257f1e75d7194b2b0fadd90e28b4fa5eef8f6
umesh <utkrisht@gmail.com>
2025-08-12 23:44:14 +0530

    user logout and porfile drop down setup after login

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -1,15 +1,53 @@
 import logo from "@/assets/images/logo-dark.png"
 import SearchBox from './SearchBox'
 import { Button } from './ui/button';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 import { CgMenu } from "react-icons/cg"; // Hamburger menu icon
 import { FaRegUserCircle } from "react-icons/fa";
 import { RiVideoAddLine } from "react-icons/ri"; // Icon for 'Create'
 import { IoMdNotificationsOutline } from "react-icons/io"; // Icon for 'Notifications'
-import { RouteSignIn } from "@/helpers/RouteName";
+import { RouteSignIn , RouteIndex} from "@/helpers/RouteName";
+import { useDispatch, useSelector } from 'react-redux';
+import {IoLogOutOutline } from 'react-icons/io5';
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import usericon from '@/assets/images/user.png'
+import { FaPlus, FaRegUser } from 'react-icons/fa';
+import { showToast } from '@/helpers/showToast';
+import { getEvn } from '@/helpers/getEnv';
+import { removeUser } from '@/redux/user/user.slice';
 
 const TopBar = ({ onToggleSidebar }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector(state => state.user)
+  const handleLogout = async() =>{
+      try {
+            const response = await fetch(`${getEvn('VITE_API_BASE_URL')}/auth/logout`, {
+                method: 'get',
+                credentials: 'include',
+                // creadentials include is neccessary to take the cookie and send it to backend
+            })
+            const data = await response.json()
+            if (!response.ok) {
+                return showToast('error', data.message)
+            }
+            dispatch(removeUser())
+            navigate(RouteIndex)
+            showToast('success', data.message)
+        } catch (error) {
+            showToast('error', error.message)
+        }
+  }
+  console.log('User state:', user);
   return (
      // Main container: Keeps everything in a row, spaced out
      <div className='flex justify-between items-center h-16 px-4 fixed bg-white w-full z-20 border-b'>
@@ -50,12 +88,49 @@ const TopBar = ({ onToggleSidebar }) => {
         </Button>
 
         {/* --- 3. SIGN IN / PROFILE BUTTON --- */}
+        {!user.isLoggedIn ?   
         <Button asChild className='bg-transparent font-bold text-blue-600 rounded-full border hover:bg-blue-100 flex items-center gap-x-2 px-3 py-2'>
             <Link to={RouteSignIn} >
                 <FaRegUserCircle size={20} />
                 <span className="hidden sm:block">Sign In</span>
             </Link>
-        </Button>
+        </Button> 
+        : <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Avatar>
+                                <AvatarImage src={user.user.avatar || usericon } />
+                            </Avatar>
+
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>
+                                <p>{user.user.username}</p>
+                                <p className='text-sm'>{user.user.email}</p>
+                            </DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem asChild className="cursor-pointer">
+                                <Link to=''>
+                                    <FaRegUser />
+                                    Profile
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem asChild className="cursor-pointer">
+                                <Link to=''>
+                                    <FaPlus />
+                                    Create Blog
+                                </Link>
+                            </DropdownMenuItem>
+
+                            <DropdownMenuSeparator />
+
+                            <DropdownMenuItem onClick={handleLogout} className="cursor-pointer">
+                                <IoLogOutOutline color='red' />
+                                Logout
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                </DropdownMenu>
+        
+        }
 
       </div>
      </div>
