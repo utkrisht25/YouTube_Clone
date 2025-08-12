aa3bdb7ad613093b136e2f27330b0bf6e700546f
umesh <utkrisht@gmail.com>
2025-08-12 13:20:14 +0530

    data base connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 
 dotenv.config();
@@ -13,6 +14,10 @@ app.use(cors({
 }))
 
 const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URI, {dbName: 'yogi-yt-clone'})
+.then(() => console.log('connected to databse'))
+.catch(err => console.log('databse connection failed', err))
 
 app.listen(PORT, () =>{
     console.log(`server is running on port ${PORT}`);
