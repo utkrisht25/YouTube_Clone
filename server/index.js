39d78aaf9578606b0c51ab9be484547906486b6c
umesh <utkrisht@gmail.com>
2025-08-13 20:57:13 +0530

    try to console the frontend url

--- server/index.js
+++ server/index.js
@@ -23,6 +23,8 @@ app.use(express.json());
 // express.json() helps us to get all the data in json format that we'll get from the frontend
 app.use(cookieParser());
 // cookie parseer will parse the data from the cookie that it will get from the frontend 
+console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
+
 app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials: true
